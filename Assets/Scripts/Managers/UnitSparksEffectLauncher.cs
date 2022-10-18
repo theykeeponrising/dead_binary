da98fe4701a6aac3c5e16ece174f81dcdaaa88a8
alexth939 <alexth939@gmail.com>
2022-10-18 21:59:42 +0300

    resolved conflicts

---
+++ Assets/Scripts/Managers/UnitSparksEffectLauncher.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public sealed class UnitSparksEffectLauncher: MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _sparksEffect;
+
+    private Unit _observedUnit;
+
+    private void Start()
+    {
+        _observedUnit = GetComponentInParent<Unit>();
+        _observedUnit.OnHealthModified += HealthModifiedHandler;
+        Debug.Log($"unit is null:{_observedUnit is null}");
+    }
+
+    private void OnDestroy()
+    {
+        _observedUnit.OnHealthModified -= HealthModifiedHandler;
+    }
+
+    private void HealthModifiedHandler()
+    {
+        int unitHealth = _observedUnit.stats.healthCurrent;
+        int maxHealth = _observedUnit.stats.healthMax;
+        bool isEffectEnabled = unitHealth < maxHealth;
+
+        _sparksEffect.SetActive(isEffectEnabled);
+    }
+}
